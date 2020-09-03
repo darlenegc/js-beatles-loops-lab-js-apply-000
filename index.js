c6213ec4d8571abc6c0cce60f041babe4ddaec17
Darlene Castro <darlenegc@gmail.com>
2020-09-03 20:35:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+
+function johnLennonFacts () {
+  var facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boyand boy scout', 'He hated the sounds of his own voice'];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + '!!!')
+    i++;
+  }
+}
