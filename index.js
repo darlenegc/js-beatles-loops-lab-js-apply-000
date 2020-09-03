3ac9acc998bcf5609a3fd023a39c029a879f08ac
Darlene Castro <darlenegc@gmail.com>
2020-09-03 20:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts () {
+  var facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian','He was a choir boy and boy scout','He hated the sound of his own voice'];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts [i] + "!!!")
+    i++;
+  }
+  return array
+}
+
+function johnLennonFacts () {
   var facts = ['While on tour, he enjoyed playing Monopoly',  'Harmonica was the first instrument Lennon learned to play'];
   var array = [];
   var i = 0;
