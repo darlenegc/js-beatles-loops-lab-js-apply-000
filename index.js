1c0b19b64ecdb4c2be81d8c44574993b34b634bd
Darlene Castro <darlenegc@gmail.com>
2020-09-03 20:42:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function johnLennonFacts () {
     i++;
   }
 }
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1 
+  } while (number < 15);
+  return array;
+}
